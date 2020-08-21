e84b45de99e32a5f8d960f03662701a16579321b
Attol8 <jacopoattolini@gmail.com>
2020-08-21 14:45:06 +0200

    discovery

--- istatapi/_nbdev.py
+++ istatapi/_nbdev.py
@@ -2,9 +2,14 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"ISTAT": "00_base.ipynb"}
+index = {"ISTAT": "00_base.ipynb",
+         "DataFlows": "01_discovery.ipynb",
+         "DataStructure": "01_discovery.ipynb",
+         "make_tree": "02_utils.ipynb",
+         "strip_ns": "02_utils.ipynb"}
 
-modules = ["base.py"]
+modules = ["base.py",
+           "utils.py"]
 
 doc_url = "https://Attol8.github.io/istatapi/"
 
