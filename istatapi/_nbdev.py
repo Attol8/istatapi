324a4c640f529240bd5ae939fb1cd781ba775a6d
Attol8 <jacopoattolini@gmail.com>
2020-08-25 11:42:11 +0200

    fixes to discovery module

--- istatapi/_nbdev.py
+++ istatapi/_nbdev.py
@@ -4,13 +4,14 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"ISTAT": "00_base.ipynb",
          "DataFlows": "01_discovery.ipynb",
-         "DataStructure": "01_discovery.ipynb",
+         "DataSet": "01_discovery.ipynb",
          "make_tree": "02_utils.ipynb",
          "strip_ns": "02_utils.ipynb"}
 
 modules = ["base.py",
            "discovery.py",
-           "utils.py"]
+           "utils.py",
+           "retrieval.py"]
 
 doc_url = "https://Attol8.github.io/istatapi/"
 
