2ac729f13a84389ab55a72859027f99032d2aa77
Attol8 <jacopoattolini@gmail.com>
2024-05-30 00:26:21 +0100

    fix retrieval

--- istatapi/base.py
+++ istatapi/base.py
@@ -7,11 +7,13 @@ __all__ = ['ISTAT', 'CustomHttpAdapter', 'get_custom_ssl_session']
 import requests
 import urllib3
 import ssl
+import warnings
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 # %% ../nbs/00_base.ipynb 4
 class ISTAT:
     """Base class that provides useful functions to communicate with ISTAT API"""
-
     def __init__(self):
         self.base_url = "http://sdmx.istat.it/SDMXWS/rest"
         self.agencyID = "IT1"
@@ -23,10 +25,17 @@ class ISTAT:
         else:
             headers = {}
         url = "/".join([self.base_url, path])
+        warnings.filterwarnings('ignore', message='Unverified HTTPS request')        
         headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
         # response = requests.get(url, headers=headers, verify = False)
         session = get_custom_ssl_session()
-        response = session.get(url, headers=headers)
+        # response = session.get(url, headers=headers, verify=False)
+        # session = requests.Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        response = session.get(url, headers=headers, verify = False)
         return response
 
 class CustomHttpAdapter (requests.adapters.HTTPAdapter):
